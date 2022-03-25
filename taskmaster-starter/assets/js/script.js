aed92e5e250a5ecff7b423a842d8b68ca51421bb
Eiko Ujifusa <eikorouji@gmail.com>
2022-03-25 11:29:35 -0700

    can edit tasks and dates, but not save the edits

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -46,8 +46,6 @@ var saveTasks = function() {
 };
 
 
-
-
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
@@ -91,7 +89,85 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+$(".list-group").on("click", "p", function() {
+  // get the textarea's current value/text //
+  var text = $(this)
+  .text()
+  .trim();
+
+// get the parent ul's id attribute //
+  var textInput = $("<textarea>")
+   .addClass("form-control")
+   .val(text);
+
+  $(this).replaceWith(textInput);
+});
+
+// get the task's position in the list of other li elements //
+var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+
+tasks[status][index].text = text;
+
+// recreate the P element //
+var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+// replace textarea with p element //
+$(this).replaceWith(taskP);
+saveTasks();
+  
 // load tasks for the first time
 loadTasks();
 
 
+// due date was clicked //
+$(".list-group").on("click", "span", function() {
+  // get current text //
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element //
+  var dateInput = $("<input")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+    // swap out elements //
+    $(this).replaceWith(dateInput);
+
+    // automatically focus on new element //
+    dateInput.trigger("focus")
+});
+
+// value of due date was changed //
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text //
+  var date = $(this)
+    .val()
+    .trim();
+
+
+ // get the parent ul's id attribute //
+ var status = $(this)
+   .closest(".list-group")
+   .attr("id")
+   .replace("list-", "");
+
+  // get the task's position in the list of other li elements //
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // recreate span element with bootstrap classes //
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  //replace input with span element
+  $(this).replaceWith(taskSpan);
+});
